60c33ee91119a020d9a4f27cb56713a72bc2e778
Binazir <binazir.farokhi@gmail.com>
2025-07-01 10:45:26 -0700

    clear

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const carb = document.getElementById("carb");
 const protein = document.getElementById("protein");
 const submit = document.getElementById("submit");
 const list = document.getElementById("list");
-
+const compare = document.getElementById("compare");
 
 class Food {
     constructor(foodName,fat,carb,protein){
@@ -17,26 +17,37 @@ class Food {
      
         const maxFat = this.fat
           
-        if ( myObject.fatname = maxFat){
+        if ( myObject[fat] > maxFat){
 
-            return `this is the highest : ${this.fat}`
+            return `this is the highest fat : ${this.fat}`
+        }
+    }
+   
+    compareCarb(myObject){
+          const maxCarb = this.carb
+          
+        if ( myObject[carb] > maxCarb){
+
+            return `this is the highest fat : ${this.carb}`
         }
 
-
     }
-    compareCarb(){
+    CompareProtein(myObject){
+  const maxprotein = this.protein
+          
+        if ( myObject[protein] > maxprotein){
 
-    }
-    CompareProtein(){
-
+            return `this is the highest fat : ${this.protein}`
+        }
     }
     getDetails (){
-        return `this is food name is ${this.foodName} and it has fat:${this.fat} and it has carb:${this.carb} and it has protein:${this.protein}`
+        return `this is food name is ${foodName} and it has fat:${this.fat} and it has carb:${this.carb} and it has protein:${this.protein}`
 
     }
 
 }
 const foods =[];
+
 submit.addEventListener("submit", function(e){
     e.preventDefault();
     const fname = foodName.value;
@@ -45,10 +56,22 @@ submit.addEventListener("submit", function(e){
           const proteinname = protein.value;
     
     const myObject = new Food(fname,fatname,carbname,proteinname)
-    console.log(myObject)
+    for (const key in myObject) {
+      console.log(`${key} and the ${myObject[key]}`)
+
+            
+        
+    }
+    console.log(myObject.CompareProtein())
     foods.push(myObject)
     console.log(foods)
    
     console.log(myObject.getDetails())
 })
 
+compare.addEventListener("click", function(){
+for (const objects of foods) {
+    objects
+    
+}
+})
